cd9473019761e1b7b0de3b1d7a2b31f2e51df65f
sankaranv <sankaranv8@gmail.com>
2018-03-14 16:19:41 +0530

    train loop

--- PA3/main.py
+++ PA3/main.py
@@ -8,23 +8,32 @@ from network import CNN
 from data import loadData
 
 # Parse args
-
+parser.add_argument('--expt_dir', default='./logs',
+                    help='save dir for experiment logs')
 parser.add_argument('--train', default='./data',
                     help='path to training set')
 parser.add_argument('--val', default='./data',
                     help='path to validation set')
 parser.add_argument('--test', default='./data',
                     help='path to test set')
+args = parser.parse_args()
 
 def parse_arch():
     pass
 
 # Load data
 train_path, valid_path, test_path = args.train, args.val, args.test
+logs_path = args.expt_dir
 data = loadData(train_path, valid_path, test_path)
 train_X, train_Y, valid_X, valid_Y, test_X, test_Y = data['train']['X'], data['train']['Y'],\
                                                      data['valid']['X'], data['valid']['Y'],\
                                                      data['test']['X'], data['test']['Y'],
+# Logging
+train_log_name = 'train.log'
+valid_log_name = 'valid.log'
+train_log = setup_logger('train-log', os.path.join(logs_path, train_log_name))
+valid_log = setup_logger('valid-log', os.path.join(logs_path, valid_log_name))
+
 # Train
 num_epochs = 1000
 batch_size = 20
@@ -33,3 +42,18 @@ steps = 0
 
 with tf.Graph().as_default(), tf.Session() as session:
     model = CNN(conv_sizes, dense_sizes, num_out, arch, session)
+    for epoch in range(num_epochs):
+        steps = 0
+        np.random.shuffle(indices)
+        train_X, train_Y = train_X[:, indices], train_Y[indices]
+        for batch in range(num_batches):
+            start, end = batch * batch_size, (batch + 1) * batch_size
+            x, y = train_X[:, range(start, end)], train_Y[range(start, end)]
+            CNN.step(x,y)
+            steps += batch_size
+            if steps % train_X.shape[1] == 0 and steps != 0:
+                train_loss, train_acc = CNN.performance(x,y)
+                train_log.info('Epoch {}, Step {}, Loss: {}, Accuracy: {}, lr: {}'.format(epoch, steps, train_loss, train_acc, CNN.lr))
+                valid_loss, valid_acc = CNN.performance(valid_X,valid_Y)
+                valid_log.info('Epoch {}, Step {}, Loss: {}, Accuracy: {}, lr: {}'.format(epoch, steps, train_loss, train_acc, CNN.lr))
+    print("Optimization Finished!")
