d71a0b053c1498ea790df38dec3bed31165d8cd8
sankaranv <sankaranv8@gmail.com>
2018-03-14 07:41:14 +0530

    defined network

--- PA3/network.py
+++ PA3/network.py
@@ -13,10 +13,25 @@ def pool2d(x,k=2):
 
 class Network:
 
-    def __init__(self, conv_sizes, fc_sizes):
+    def __init__(self, conv_sizes, dense_sizes, num_out, initializer='xavier'):
         self.params = {}
+        # Initializers
+        if initializer='he':
+            init = tf.contrib.layers.variance_scaling_initializer()
+        else:
+            init = tf.contrib.layers.xavier_initializer()
+        # Conv weights
         for i in range(len(conv_sizes)):
             weight = 'Wc{}'.format(i)
-            self.params[weight] = tf.Variable(tf.random_normal(conv_sizes[i]))
+            self.params[weight] = tf.get_variable(name = weight, shape = conv_sizes[i], initializer = init)
             bias = 'bc{}'.format(i)
-            self.params[bias] = tf.Variable(tf.random_normal([conv_sizes[i][3]]))
+            self.params[bias] = tf.get_variable(name = bias, shape = [conv_sizes[i][3]], initializer = init)
+        # Dense weights
+        for i in range(1,len(dense_sizes)):
+            weight = 'Wd{}'.format(i)
+            self.params[weight] = tf.get_variable(name = weight, shape = [dense_sizes[i],dense_sizes[i-1]], initializer = init)
+            bias = 'bd{}'.format(i)
+            self.params[bias] = tf.get_variable(name = weight, shape = [dense_sizes[i],1], initializer = init)
+        # Output layer weight
+        self.params['Wout'] = tf.get_variable(name = 'Wout', shape = [num_out,dense_sizes[-1]], initializer = init)
+        self.params['bout'] = tf.get_variable(name = 'bout', shape = [num_out,1], initializer = init)
