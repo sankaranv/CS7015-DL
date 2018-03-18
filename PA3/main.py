ac1c69ababd5b1dbf5706e364b6bc67d0de7ef16
cs16s027 <cs16s027@smail.iitm.ac.in>
2018-03-18 14:09:59 +0530

    Changed test()

--- PA3/main.py
+++ PA3/main.py
@@ -1,4 +1,5 @@
 import sys, os
+import resource
 import argparse
 import logging
 from helpers import setup_logger
@@ -10,70 +11,113 @@ from data import loadData
 from network import CNN
 from modelparser import loadArch
 
-# Parse args
-parser = argparse.ArgumentParser(description='Train the MLP')
-parser.add_argument('--expt_dir', default='./logs',
-                    help='save dir for experiment logs')
-parser.add_argument('--train', default='./data',
-                    help='path to training set')
-parser.add_argument('--val', default='./data',
-                    help='path to validation set')
-parser.add_argument('--test', default='./data',
-                    help='path to test set')
-parser.add_argument('--save_dir', default='./models',
-                    help='path to save model')
-args = parser.parse_args()
+def memory_limit():
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    resource.setrlimit(resource.RLIMIT_AS, (get_memory() * 1024 / 2, hard))
 
-# Load data
-train_path, valid_path, test_path = args.train, args.val, args.test
-logs_path = args.expt_dir
-model_path = args.save_dir
-data = loadData(train_path, valid_path, test_path)
-train_X, train_Y, valid_X, valid_Y, test_X, test_Y = data['train']['X'], data['train']['Y'],\
-                                                     data['valid']['X'], data['valid']['Y'],\
-                                                     data['test']['X'], data['test']['Y'],
-# Logging
-train_log_name = 'train.log'
-valid_log_name = 'valid.log'
-train_log = setup_logger('train-log', os.path.join(logs_path, train_log_name))
-valid_log = setup_logger('valid-log', os.path.join(logs_path, valid_log_name))
+def get_memory():
+    with open('/proc/meminfo', 'r') as mem:
+        free_memory = 0
+        for i in mem:
+            sline = i.split()
+            if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
+                free_memory += int(sline[1])
+    return free_memory
 
-# Train
-num_epochs = 100
-batch_size = 20
-num_batches = int(float(train_X.shape[0]) / batch_size)
-steps = 0
-patience = 5
-early_stop=0
-# Load architecture
-arch = loadArch('models/cnn.json')
+# Test model on data after every epoch
+def test(model, x, y, epoch, batch_size, is_train):
+    print 'Testing for epoch {}'.format(epoch)
+    num_batches = x.shape[0] / batch_size
+    loss, acc = np.zeros((num_batches)), np.zeros((num_batches))
+    for batch in range(num_batches):
+        start, end = batch * batch_size, (batch + 1) * batch_size
+        batch_x, batch_y = x[start : end], y[start : end]
+        idx = epoch*num_batches + batch
+        loss[batch], acc[batch] = model.performance(batch_x, batch_y, is_train, idx)
+    return np.mean(loss), np.mean(acc)
 
-model_name = 'cnn'
-with tf.Graph().as_default(), tf.Session() as session:
-    model = CNN(arch, session, logs_path)
-    loss_history = [np.inf]
-    for epoch in range(num_epochs):
-        steps = 0
-        indices = np.arange(train_X.shape[0])
-        np.random.shuffle(indices)
-        train_X, train_Y = train_X[indices], train_Y[indices]
-        for batch in range(num_batches):
-            start, end = batch * batch_size, (batch + 1) * batch_size
-            x, y = train_X[range(start, end)], train_Y[range(start, end)]
-            idx = epoch*num_batches + batch
-            model.step(x,y)
-            steps += batch_size
-            if steps % train_X.shape[0] == 0 and steps != 0:
-                train_loss, train_acc = model.performance(train_X, train_Y, idx)
-                train_log.info('Epoch {}, Step {}, Loss: {}, Accuracy: {}, lr: {}'.format(epoch, steps, train_loss, train_acc, model.lr))
-                valid_loss, valid_acc = model.performance(valid_X, valid_Y, idx)
-                valid_log.info('Epoch {}, Step {}, Loss: {}, Accuracy: {}, lr: {}'.format(epoch, steps, valid_loss, valid_acc, model.lr))
-                if valid_loss < min(loss_history):
-                    model.save(os.path.join(model_path, model_name))
-                    early_stop = 0
-                early_stop += 1
-                if (early_stop >= patience):
-                    print "No improvement in validation loss for " + str(patience) + " steps - stopping training!"
-                    break
-                loss_history.append(valid_loss)
-    print("Optimization Finished!")
+def train():
+    # Parse args
+    parser = argparse.ArgumentParser(description='Train the CNN')
+    parser.add_argument('--expt_dir', default='./logs',
+                        help='save dir for experiment logs')
+    parser.add_argument('--train', default='./data',
+                        help='path to training set')
+    parser.add_argument('--val', default='./data',
+                        help='path to validation set')
+    parser.add_argument('--test', default='./data',
+                        help='path to test set')
+    parser.add_argument('--save_dir', default='./models',
+                        help='path to save model')
+    args = parser.parse_args()
+
+    # Load data
+    train_path, valid_path, test_path = args.train, args.val, args.test
+    logs_path = args.expt_dir
+    model_path = args.save_dir
+    data = loadData(train_path, valid_path, test_path)
+    train_X, train_Y, valid_X, valid_Y, test_X, test_Y = data['train']['X'], data['train']['Y'],\
+                                                         data['valid']['X'], data['valid']['Y'],\
+                                                         data['test']['X'], data['test']['Y'],
+    # Logging
+    train_log_name = 'train.log'
+    valid_log_name = 'valid.log'
+    train_log = setup_logger('train-log', os.path.join(logs_path, train_log_name))
+    valid_log = setup_logger('valid-log', os.path.join(logs_path, valid_log_name))
+
+    # Train
+    num_epochs = 100
+    batch_size = 20
+    num_batches = int(float(train_X.shape[0]) / batch_size)
+    steps = 0
+    patience = 5
+    early_stop=0
+    # Load architecture
+    arch = loadArch('models/cnn.json')
+            
+    model_name = 'cnn'
+    with tf.Graph().as_default(), tf.Session() as session:
+        model = CNN(arch, session, logs_path)
+        loss_history = [np.inf]
+        for epoch in range(num_epochs):
+            steps = 0
+            indices = np.arange(train_X.shape[0])
+            np.random.shuffle(indices)
+            train_X, train_Y = train_X[indices], train_Y[indices]
+            for batch in range(num_batches):
+                print 'Epoch {}, batch {}'.format(epoch, batch)
+                start, end = batch * batch_size, (batch + 1) * batch_size
+                x, y = train_X[range(start, end)], train_Y[range(start, end)]
+                try:
+                    model.step(x,y)
+                except MemoryError:
+                    print 'Memory error in step'
+                    exit()
+                steps += batch_size
+                if steps % train_X.shape[0] == 0 and steps != 0:
+                    try:
+                        train_loss, train_acc = test(model, train_X, train_Y, epoch, batch_size, True)
+                    except MemoryError:
+                        print 'Memory error in test for train'
+                        exit()
+                    train_log.info('Epoch {}, Step {}, Loss: {}, Accuracy: {}, lr: {}'.format(epoch, steps, train_loss, train_acc, model.lr))
+                    try:
+                        valid_loss, valid_acc = test(model, valid_X, valid_Y, epoch, batch_size, False)               
+                    except MemoryError:
+                        print 'Memory error in test for valid'
+                        exit()
+                    valid_log.info('Epoch {}, Step {}, Loss: {}, Accuracy: {}, lr: {}'.format(epoch, steps, valid_loss, valid_acc, model.lr))
+                    if valid_loss < min(loss_history):
+                        model.save(os.path.join(model_path, model_name))
+                        early_stop = 0
+                    early_stop += 1
+                    if (early_stop >= patience):
+                        print "No improvement in validation loss for " + str(patience) + " steps - stopping training!"
+                        break
+                    loss_history.append(valid_loss)
+        print("Optimization Finished!")
+
+if __name__ == '__main__':
+    memory_limit()
+    train()
+			
