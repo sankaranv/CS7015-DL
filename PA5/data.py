c63d5cc3fab2b43c17263aab3ecdc4f336a0e086
Karthik Thiagarajan <cs16s027@smail.iitm.ac.in>
2018-04-29 21:42:34 +0530

    Working code for CD
    Loss is decreasing

---
+++ PA5/data.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+
+
+def loadFromCSV(fname, is_test = False):
+    lines = [line.strip().split(',') for line in open(fname, 'r').readlines()][1 : ]
+    size = len(lines)
+    dim = 784
+    scale = 255.0
+    X, Y = np.zeros((size, dim), dtype = np.float32), np.zeros((size), dtype = np.int32)
+    for index, line in enumerate(lines):
+        if is_test == False:
+            Y[index] = np.int32(line[-1])
+        X[index][:] = np.float32([0 if float(pixel) < 127 else 1 for pixel in line[1 : 1 + dim]])
+    print 'Loaded data of shape', X.shape, Y.shape
+    return X, Y
+
+def loadData(train_path, valid_path, test_path):
+    stages = ['train', 'test']
+    normalize = True
+    data_path = 'data'
+    if os.path.isfile(os.path.join(data_path, 'train_X.npy')) == False:
+        train_X, train_Y = loadFromCSV(train_path)
+        valid_X, valid_Y = loadFromCSV(valid_path)
+        test_X, test_Y   = loadFromCSV(test_path)
+
+        data = {'train' : {'X' : np.concatenate([train_X, valid_X], axis = 0), 'Y' : np.concatenate([train_Y, valid_Y], axis = 0)},\
+                'test'  : {'X' : test_X, 'Y'  : test_Y}}
+
+        # Shuffle training data
+        indices = np.arange(data['train']['X'].shape[0])
+        np.random.seed(1234)
+        np.random.shuffle(indices)
+        data['train']['X'], data['train']['Y'] = data['train']['X'][indices], data['train']['Y'][indices]
+
+        for stage in stages:
+            np.save(os.path.join(data_path, '{}_X.npy'.format(stage)), data[stage]['X'])
+            np.save(os.path.join(data_path, '{}_Y.npy'.format(stage)), data[stage]['Y'])
+
+    data = {'train' : {'X' : None, 'Y' : None,},\
+            'test'  : {'X' : None, 'Y' : None}}
+
+    # Load data, shape of returned array is (num_points, dimension) : each row is a data-point
+    for stage in stages:
+        data[stage]['X'] = np.load(os.path.join(data_path, '{}_X.npy'.format(stage)))
+        data[stage]['Y'] = np.load(os.path.join(data_path, '{}_Y.npy'.format(stage)))
+
+    print 'Loaded train-val-test data'
+
+    return data
+
+if __name__ == '__main__':
+
+    loadData('data/train.csv', 'data/val.csv', 'data/test.csv')
+    pass
--- PA5/rbm.py
+++ PA5/rbm.py
@@ -1,58 +1,91 @@
 import numpy as np
+import h5py
+from data import loadData
+
+def sigmoid(x):
+    #print 1.0 / (1.0 + np.exp(-x))
+    return 1.0 / (1.0 + np.exp(-x))
 
 class RBM:
 
-	def __init__(self, num_V, num_H):
-		
-		self.num_V = num_V
-		self.num_H = num_H
+    def __init__(self, num_V, num_H):
+
+        self.num_V = num_V
+        self.num_H = num_H
 
 		#Weights and Biases
 		#Refer Hinton's practical guide to training RBMs
-		self.W = np.random.normal(0.0,0.01,(num_V,num_H))
-		self.b = np.zeros(num_V)
-		self.c = np.zeros(num_H)
+        #self.W = np.random.normal(0.0, 0.001, (num_V,num_H))
+        self.W = np.zeros((num_V, num_H))
+        self.b = np.zeros((1, num_V))
+        self.c = np.zeros((1, num_H))
 
-	def train_contrastive_divergence(self, k, data, batch_size, lr):
-		
-		num_iter = data.shape[0]/batch_size
-		for d in range(num_iter):
-			V0 = data[d*batch_size:(d+1)*batch_size]		
-			#Positive Sample
-			pos_H = block_gibbs_sample_HgivenV(init_V = V0)
-			pos_V = V0
-			#Negative Sample
-			neg_V = np.copy(V0)
-			for i in range(k):
-				neg_H=block_gibbs_sample_HgivenV(init_V = neg_V)
-				neg_V=block_gibbs_sample_VgivenH(init_H = neg_H)	
-			neg_H=block_gibbs_sample_HgivenV(init_V = neg_V)
-
-			self.W += self.lr * (np.dot(pos_H, pos_V.T) - np.dot(neg_H, neg_V.T))
-			self.b += self.lr * (pos_V - neg_V)
-			self.c += self.lr * (pos_H - neg_H)
-
-
-	def block_gibbs_sample_VgivenH(init_H):
-		expected_V = np.dot(init_H.T,self.W).T+self.b
-        #Sigmoid
-        expected_V = np.exp(expected_V)
-        expected_V /= (1.+expected_V)
+    def block_gibbs_sample_VgivenH(self, init_H):
+        # Probability of V being 1 given H
+        expected_V = sigmoid(np.dot(init_H, self.W.T) + self.b)
         #Bernoulli Sampling
-        rand_vec = np.random.rand(self.num_V,1)
-        sample_V = int(rand_vec<expected_V)
+        batch_size, size = expected_V.shape
+        rand_vec = np.random.rand(batch_size, size)
+        sample_V = np.int32(rand_vec < expected_V)
         return sample_V
 
-
-	def block_gibbs_sample_HgivenV(init_V):
-		expected_H = np.dot(self.W, init_V)+self.c
-		#Sigmoid
-		expected_H = np.exp(expected_H)
-		expected_H /= (1.+expected_H)
+    def block_gibbs_sample_HgivenV(self, init_V):
+        # Probability of H being 1
+        expected_H = sigmoid(np.dot(init_V, self.W) + self.c)
 		#Bernoulli Sampling
-		rand_vec = np.random.rand(self.num_H,1)
-		sample_H = int(rand_vec<expected_H)
-		return sample_H
+        batch_size, size = expected_H.shape
+        rand_vec = np.random.rand(batch_size, size)
+        sample_H = np.int32(rand_vec < expected_H)
+        return sample_H
 
+    def train_contrastive_divergence(self, data, k = 20, batch_size = 32, lr = 0.01, epochs = 10, save_path = None):
+        num_batches = data.shape[0] / batch_size
+        loss_history = [np.inf]
+        for epoch in range(epochs):
+            print 'Epoch-{}'.format(epoch + 1)
+            for batch in range(num_batches):
+                V_data = data[batch * batch_size : (batch + 1) * batch_size, :]
+                V_model = np.copy(V_data)
+                for t in range(k):
+                    H_model = self.block_gibbs_sample_HgivenV(V_model)
+                    V_model = self.block_gibbs_sample_VgivenH(H_model)
+                grad_W = np.dot(V_data.T, sigmoid(np.dot(V_data, self.W) + self.c))\
+                                - np.dot(V_model.T, sigmoid(np.dot(V_model, self.W) + self.c))
+                grad_b = (V_data - V_model).sum(axis = 0)
+                grad_c = (sigmoid(np.dot(V_data, self.W) + self.c) - sigmoid(np.dot(V_model, self.W) + self.c)).sum(axis = 0)
+                self.W += 1.0 / batch_size * lr * grad_W
+                self.b += 1.0 / batch_size * lr * grad_b
+                self.c += 1.0 / batch_size * lr * grad_c
+            H_model = self.block_gibbs_sample_HgivenV(data)
+            V_model = self.block_gibbs_sample_VgivenH(H_model)
+            loss = 1.0 / data.shape[0] * np.power((V_model - data), 2).sum()
+            if loss < np.min(loss_history):
+                self.saveModel(save_path)
+            else:
+                lr *= 0.5
+            loss_history.append(loss)
+            print 'Epoch {}: {}'.format(epoch + 1, loss)
 
-		
+    def saveModel(self, save_path):
+        with h5py.File(save_path, 'w') as f:
+            f.create_dataset(data = self.W, name = 'W', shape = self.W.shape, dtype = self.W.dtype)
+            f.create_dataset(data = self.b, name = 'b', shape = self.b.shape, dtype = self.b.dtype)
+            f.create_dataset(data = self.c, name = 'c', shape = self.c.shape, dtype = self.c.dtype)
+
+    def loadModel(self, load_path):
+        with h5py.File(load_path, 'r') as f:
+            self.W = f['W'][:]
+            self.b = f['b'][:]
+            self.c = f['c'][:]
+
+if __name__ == '__main__':
+    train_data = loadData('data/train.csv', 'data/val.csv', 'data/test.csv')['train']['X']
+    rbm = RBM(784, 100)
+    # Hyperparams
+    k = 1
+    batch_size = 50
+    lr = 0.1
+    epochs = 20
+    save_path = '{}-{}-{}'.format(k, batch_size, epochs)
+    rbm.train_contrastive_divergence(train_data, k = k, batch_size = batch_size, lr = lr, epochs = epochs,\
+                                     save_path = save_path)
